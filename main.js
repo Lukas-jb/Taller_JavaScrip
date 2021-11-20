8c086fd1bf694c320e9d69fd4d4d8ee7c01d323a
Lukas-jb <92315994+Lukas-jb@users.noreply.github.com>
2021-11-20 15:46:34 -0500

    Se crean las barras

--- main.js
+++ main.js
@@ -4,10 +4,11 @@
     self.Board = function (width, height) {
 		this.width = width;
 		this.height = height;
-		this.game_over = false;
+		this.playing = true;
+        this.game_over = false;
 		this.bars = [];
 		this.ball = null;
-		this.playing = true;
+		
 	}
     self.Board.prototype = {
 		get elements() {
@@ -19,14 +20,64 @@
 })();
 
 (function () {
+	self.Bar = function (x, y, width, height, board) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.board = board;
+		this.board.bars.push(this); // Se agrega la barra al tablero
+		this.kind = 'rectangle'; // Se define el tipo de elemento
+		
+	}
+	self.Bar.prototype = {
+		down: function () {
+          this.y +=speed;                                                                //pendiente
+		},
+		up: function () {                                                               //pendiente
+			this.y-speed;
+		},
+		
+	}
+})();
+
+(function () {
 	self.BoardView = function (canvas, board) {
 		this.canvas = canvas;
 		this.canvas.width = board.width;
 		this.canvas.height = board.height;
 		this.board = board; // Se asigna el tablero
 		this.ctx = this.canvas.getContext('2d'); // Obtiene el contexto del canvas
-		this.canvas.style.background = '#955227';
+		//this.canvas.style.background = '#955227';
 	}
+      
+    self.BoardView.prototype = {
+        
+        draw: function () {
+			for (var i = this.board.elements.length - 1; i >= 0; i--) { // Se recorre el elemento para dibujarlo
+				var el = this.board.elements[i];
+				
+
+				draw(this.ctx, el);
+			}
+		},
+
+    }
+
+    // Se encarga de dibujar el element elegido
+	function draw(ctx, element) {
+    
+        if(element !== null && element.hasOwnProperty("kind")){
+         switch (element.kind) {
+			case 'rectangle':
+				ctx.fillRect(element.x, element.y, element.width, element.height);
+				break;
+			
+		}   
+        }
+		
+	}
+
 })();
 
 
@@ -39,5 +90,7 @@ window.addEventListener('load', main)
         var bar_2 = new Bar(780, 150, 20, 100, board); // Se crea la barra2
         var canvas = document.getElementById('canvas'); // Se obtiene el canvas desde el DOM
         var board_view = new BoardView(canvas, board); // Se crea el tablero
-        var ball = new Ball(400, 200, 10, board); // Se crea la pelota 
+        //var ball = new Ball(400, 200, 10, board); // Se crea la pelota 
+        console.log(Board);
+        board_view.draw();
     }
